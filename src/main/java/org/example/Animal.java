2d7022d657a20646adaabb3c7169f89507ee03dc
Alkey <Sirj777sirj@gmail.com>
2020-07-21 10:38:09 +0300

    rename

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Animal {
+    String name;
+    int size;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
