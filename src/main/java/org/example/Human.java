753550674317f66f08e050877f7cb2d2eaf05812
Alkey <Sirj777sirj@gmail.com>
2020-07-21 10:34:33 +0300

    rename

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Human {
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.age = 18;
+        human.name = "Ivan";
+    }
+}
